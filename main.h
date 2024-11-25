a1741548d0fc405455fa03752980fc9bdd85759f
Albat93 <albat93120@gmail.com>
2024-11-25 14:37:01 +0100

    adding proto of specifier folder

--- main.h
+++ main.h
@@ -11,9 +11,14 @@
 typedef struct _printf
 {
 	char *spec;
-	int (*print)(va_list ap);
+	int (*print)(va_list ap, int);
 } print;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_percent(va_list ap);
+int print_int(va_list ap);
+int print_decimal(va_list ap);
+int print_string(va_list ap);
+int print_char(va_list ap);
 #endif
