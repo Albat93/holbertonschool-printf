917823f276774f9e1acd780a3dcd84491337de92
Albat93 <albat93120@gmail.com>
2024-11-25 12:17:41 +0100

    specifier folder to complete

--- specifiers.c
+++ specifiers.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+int print_char(va_list ap)
+{}
+
+int print_string(va_list ap)
+{}
+
+int print_decimal(va_list ap)
+{}
+
+int print_int(va_list ap)
+{}
+
+int print_percent(va_list ap)
+{}
