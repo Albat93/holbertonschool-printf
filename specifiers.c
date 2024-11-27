605ab898b4c683c190b92622c822a700c1bb4a60
Albat93 <albat93120@gmail.com>
2024-11-27 10:40:25 +0100

    add function decimal

--- specifiers.c
+++ specifiers.c
@@ -5,28 +5,61 @@
 
 /**
  */
-void printf_char(va_list ap)
+int printf_char(va_list ap)
 {
 	char c = va_arg(ap, int);
 		_putchar(c);
+	return (1);
 }
 
 /**
  */
-void printf_string(va_list ap)
+int printf_string(va_list ap)
 {
 	char *str = va_arg(ap, char *);
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		i++;
+		_putchar(str[i]);
 	}
-	_putchar(i);
+	return (1);
 }
-void printf_percent(va_list ap)
+
+int printf_percent(va_list ap)
 {
-	char c = va_arg(ap, int);
-	if (c == '%')
-		_putchar(37);
+	(void) ap;
+	_putchar ('%');
+	return (1);
+}
+
+int printf_decimal(va_list ap)
+{
+	int n = va_arg(ap, int);
+	int j = 0;
+	char buffer[10];
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		buffer[j] = (n % 10) + '0';
+		j++;
+		n /= 10;
+	}
+	for (j = j - 1; j >= 0; j--)
+	{
+		_putchar(buffer[j]);
+	}
+
+	return (1);
+
 }
