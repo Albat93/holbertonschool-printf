104f68b24631ee9da5f65b2e6641372b2a8a2e2e
Albat93 <albat93120@gmail.com>
2024-11-27 10:41:08 +0100

    modify type void to int for proto and type

--- main.h
+++ main.h
@@ -14,14 +14,14 @@
 typedef struct type_spec
 {
 	char *spec;
-	void (*func)(va_list ap);
+	int (*func)(va_list ap);
 } type_spec;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-void printf_percent(va_list ap);
-void printf_int(va_list ap);
-void printf_decimal(va_list ap);
-void printf_string(va_list ap);
-void printf_char(va_list ap);
+int printf_percent(va_list ap);
+int printf_int(va_list ap);
+int printf_decimal(va_list ap);
+int printf_string(va_list ap);
+int printf_char(va_list ap);
 #endif
