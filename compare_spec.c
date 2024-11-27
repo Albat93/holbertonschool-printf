1d2afc6634f00eabbb91bccf537d090b7f6d2961
Albat93 <albat93120@gmail.com>
2024-11-27 22:04:34 +0100

    add compare_spec folder

---
+++ compare_spec.c
@@ -0,0 +1,39 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * compare_spec - compare specifier with function associated
+ * @format_spec: specifier to check
+ * @ap: La liste d'arguments (va_list)
+ *
+ * Return: the number of char printed. or 0 if wrong specifier
+ */
+int compare_spec(char format_spec, va_list ap)
+{
+	int j;
+	int count = 0;
+
+	type_spec spec[] = {
+		{"c", printf_char},
+		{"s", printf_string},
+		{"%", printf_percent},
+		{"d", printf_decimal},
+		{"i", printf_decimal},
+		{"u", printf_unsigned},
+		{"x", printf_hex_lower},
+		{"X", printf_hex_upper},
+		{NULL, NULL},
+	};
+	j = 0;
+	while (spec[j].spec)
+	{
+		if (format_spec == spec[j].spec[0])
+		{
+			count = spec[j].func(ap)
+			return (count);
+		}
+		j++;
+	}
+
+	return (0);
+}
