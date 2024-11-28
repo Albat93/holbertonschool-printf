b19781ab69d39c479f5f59fe88decccae4629ffd
Albat93 <albat93120@gmail.com>
2024-11-28 15:16:31 +0100

    back to previous version

--- _printf.c
+++ _printf.c
@@ -8,32 +8,33 @@
  * @format: Types of arguments passed to function
  * Return: matches specifier with the right function to print
  */
+
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i = 0, count = 0, r_spec = 0;
+	int i = 0;
+	int count = 0;
 
 	va_start(ap, format);
-
 	if (*format == '\0')
 	{
 		va_end(ap);
 		return (-1);
 	}
-
 	while (format && format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			r_spec += compare_spec(format[i + 1], ap);
-			if (r_spec == 0)
-			{
-				_putchar(format[i]);
-				_putchar(format[i + 1]);
-				count += 2;
-			}
-			count += r_spec;
 			i++;
+			if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				count += compare_spec(format[i], ap);
+			}
 		}
 		else
 		{
