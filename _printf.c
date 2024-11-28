3a361157ecdae21004bba3c555a16ff7cf055548
Albat93 <albat93120@gmail.com>
2024-11-28 13:31:39 +0100

    final version of printf

--- _printf.c
+++ _printf.c
@@ -8,88 +8,14 @@
  * @format: Types of arguments passed to function
  * Return: matches specifier with the right function to print
  */
-/**
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i = 0, j;
-	int count = 0;
-
-	type_spec spec[] = {
-		{"c", printf_char},
-		{"s", printf_string},
-		{"%", printf_percent},
-		{"d", printf_decimal},
-		{"i", printf_decimal},
-		{NULL, NULL},
-	};
-	va_start(ap, format);
-
-	if (*format == '\0')
-	{
-		return (-1);
-	}
-	while (format && format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				j = 0;
-				while (spec[j].spec)
-				{
-					if (format[i] == spec[j].spec[0])
-					{
-						spec[j].func(ap);
-						count++;
-						break;
-					}
-					j++;
-				}
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
-	}
-
-	va_end(ap);
-	return (count);
-}
-*/
-
-/**
-
-int main(void)
-{
-	int count;
-
-	count = _printf("Character: %c, String: %s, Decimal: %d\n", 'A', "Hello World!", 123);
-	_printf("Total printed characters: %d\n", count);
-
-
-
-	return 0;
-}
-*/
-int _printf(const char *format, ...)
-{
-	va_list ap;
-	int i = 0;
-	int count = 0;
+	int i = 0, count = 0;
 
 	va_start(ap, format);
 
-	if (*format == '\0')
+	if (!format)
 	{
 		va_end(ap);
 		return (-1);
@@ -99,16 +25,14 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
+			if (!compare_spec(format[i + 1], ap))
+			{
+				_putchar(format[i]);
+				_putchar(format[i + 1]);
+				count += 2;
+			}
+			count++;
 			i++;
-			if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				count += compare_spec(format[i], ap);
-			}
 		}
 		else
 		{
@@ -120,4 +44,3 @@ int _printf(const char *format, ...)
 	va_end(ap);
 	return (count);
 }
-
